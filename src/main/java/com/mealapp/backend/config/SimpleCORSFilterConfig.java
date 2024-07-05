7918fb2089b432ae36faf8e51d02f64151018947
Soham <Sohamrana77@gmail.com>
2024-07-05 16:26:25 +0530

    Control allow

--- src/main/java/com/mealapp/backend/config/SimpleCORSFilterConfig.java
+++ src/main/java/com/mealapp/backend/config/SimpleCORSFilterConfig.java
@@ -1,6 +1,5 @@
 package com.mealapp.backend.config;
 
-
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -19,7 +18,6 @@ public class SimpleCORSFilterConfig implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "authorization, content-type, xsrf-token");
